7a1b6fd373623b91c06ceddfdc067ef56d827254
pcm <filsamare@gmail.com>
2026-02-11 10:55:08 +0700

    Clear interrupts + Setup GDT + GDB debugging After exiting boot services, the kernel now clears the interrupt flag, which prevents any unexpected interrupts as the Interrupt Descriptor Table (IDT) isn't set up yet. Afterwards, the kernel will setup a simple GDT with a code and data segment. Also there is a new script, "run_debug.bat" that executes qemu with GDB debugging in mind.

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 
 if not exist build mkdir build
 
-set CompileFlags=-std=c11 -Wall -Wextra -Wpedantic -O0 -target x86_64-unknown-windows -ffreestanding -mno-red-zone -mno-stack-arg-probe -nostdlib -fuse-ld=lld -nostdlib
+set CompileFlags=-std=c11 -Wall -Wextra -Wpedantic -g -O0 -target x86_64-unknown-windows -ffreestanding -mno-red-zone -mno-stack-arg-probe -nostdlib -fuse-ld=lld -nostdlib
 set LinkFlags=-Wl,-nodefaultlib,-subsystem:efi_application,-entry:UEFIBoot
 
 pushd build
